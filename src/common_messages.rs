dcb1e144714b0eeb7813399073eaffc09c7ce4df
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-08-03 10:47:00 -0400

    Refact TransactionCle, RequeteVerifierPreuve vers common

---
+++ src/common_messages.rs
@@ -0,0 +1,69 @@
+use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
+use std::error::Error;
+
+use crate::chiffrage::{CommandeSauvegarderCle, FormatChiffrage};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct TransactionCle {
+    pub cle: String,
+    pub domaine: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub partition: Option<String>,
+    pub format: FormatChiffrage,
+    pub hachage_bytes: String,
+    pub identificateurs_document: HashMap<String, String>,
+    pub iv: String,
+    pub tag: String,
+}
+
+impl TransactionCle {
+    pub fn new_from_commande(commande: &CommandeSauvegarderCle, fingerprint: &str)
+        -> Result<Self, Box<dyn Error>>
+    {
+        let cle = match commande.cles.get(fingerprint) {
+            Some(c) => c,
+            None => {
+                Err(format!("TransactionCle.new_from_commande Cle non trouvee pour fingerprint {}", fingerprint))?
+            }
+        };
+
+        Ok(TransactionCle {
+            cle: cle.to_owned(),
+            domaine: commande.domaine.clone(),
+            partition: commande.partition.clone(),
+            format: commande.format.clone(),
+            hachage_bytes: commande.hachage_bytes.to_owned(),
+            identificateurs_document: commande.identificateurs_document.clone(),
+            iv: commande.iv.clone(),
+            tag: commande.tag.clone(),
+        })
+    }
+
+    pub fn into_commande<S>(self, fingerprint: S) -> CommandeSauvegarderCle
+        where S: Into<String>
+    {
+        let fingerprint_ = fingerprint.into();
+        let mut cles: HashMap<String, String> = HashMap::new();
+        cles.insert(fingerprint_, self.cle);
+        CommandeSauvegarderCle {
+            cles,
+            domaine: self.domaine,
+            partition: self.partition,
+            format: self.format,
+            hachage_bytes: self.hachage_bytes,
+            identificateurs_document: self.identificateurs_document,
+            iv: self.iv,
+            tag: self.tag,
+            fingerprint_partitions: None
+        }
+    }
+
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct RequeteVerifierPreuve {
+    // cles: HashMap<String, String>,
+    pub cles: Vec<TransactionCle>,
+    // domaine: Option<String>,
+}
