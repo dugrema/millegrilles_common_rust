47e196662225d607c6b5dee7d7e993ecd4f370cc
Mathieu Dugre <mathieu.dugre@mdugre.info>
2026-08-19 18:44:58 -0400

    Removed get_reply_q_name from trait (internal only)

--- src/v3/impls/messaging_service.rs
+++ src/v3/impls/messaging_service.rs
@@ -86,10 +86,6 @@ impl MessagingService for MessagingServiceImpl {
         }
     }
 
-    fn get_reply_q_name(&self) -> Option<String> {
-        self.queue_registry.get_reply_q_name()
-    }
-
     fn take_named_q_rx(&self, q_name: &str) -> Result<Receiver<MessageMilleGrillesBufferDefault>, CommonError> {
         self.queue_registry.take_named_q_rx(q_name)
     }
